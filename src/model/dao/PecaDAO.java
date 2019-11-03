1ff8387e6debc385ca5a22b728b192662b03ed70
smmac <smmac@DESKTOP-23FJSDK>
2019-11-03 01:52:17 -0300

    Inclusáo peca e classe banco

--- src/model/dao/PecaDAO.java
+++ src/model/dao/PecaDAO.java
@@ -1,14 +1,68 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import model.entity.Peca;
 
 public class PecaDAO implements BaseDAO<Peca> {
 
 	@Override
 	public Peca cadastrar(Peca peca) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		Connection conn = Banco.getConnection();
+		String sql = "INSERT INTO PECA (NOMEPECA, CODIGO ) "
+				+ "VALUES (?,?)";
+		PreparedStatement stmt = Banco.getPreparedStatement(conn, sql, 
+				PreparedStatement.RETURN_GENERATED_KEYS);
+		
+		try {
+			stmt.setString(1, peca.getNomePeca());
+			stmt.setString(2, peca.getCodigo());
+			
+			
+			stmt.execute();
+			
+			ResultSet generatedKeys = stmt.getGeneratedKeys();
+			if(generatedKeys.next()) {
+				int idGerado = generatedKeys.getInt(1);
+				peca.setIdPeca(idGerado);
+			
+			//Adiciona Item Peça
+				sql = "INSERT INTO ITEM_PECA (IDPECA, QUANTIDADE, VALORCOMPRA, VALORVENDA, DATAENTRADA, DATASAIDA ) "
+						+ "VALUES (?,?,?,?,?,?)";
+				stmt = Banco.getPreparedStatement(conn, sql, 
+						PreparedStatement.RETURN_GENERATED_KEYS);
+				try { 
+					stmt.setInt(1, peca.getIdPeca());
+					stmt.setInt(2, peca.getQuantida());
+					stmt.setDouble(3, peca.getValCompra());
+					stmt.setDouble(4, peca.getValVenda());
+					stmt.setString(5, peca.getDataEntrada());
+					stmt.setString(6, peca.getDataSaida());
+					
+					stmt.execute();
+				
+			
+				
+			}finally {
+				generatedKeys = stmt.getGeneratedKeys();
+				if(generatedKeys.next()) {
+					idGerado = generatedKeys.getInt(1);
+					peca.setIdItempeca(idGerado);
+				}
+			}
+					
+		} }catch (SQLException e) {
+			System.out.println("Erro ao inserir nova Peça.");
+			System.out.println("Erro: " + e.getMessage());
+		}
+		
+
+		return peca;
+		}
+	
 
 	@Override
 	public Peca consultar(Peca peca) {
--- src/model/entity/Peca.java
+++ src/model/entity/Peca.java
@@ -1,19 +1,31 @@
 package model.entity;
 
+
 public class Peca {
 	
 	private String nomePeca;
 	private String codigo;
+	private double valCompra;
+	private double valVenda;
+	private String dataEntrada;
+	private String dataSaida;
+	private int quantida;
+	private int idPeca;
+	private int idItempeca;
 	
-	public Peca(String nomePeca, String codigo) {
-		super();
-		this.nomePeca = nomePeca;
-		this.codigo = codigo;
-	}
-
 	public Peca() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+
+	public Peca(String nomePeca, String codigo, double valCompra, double valVenda, String dataEntrada, int quantida) {
+		super();
+		this.nomePeca = nomePeca;
+		this.codigo = codigo;
+		this.valCompra = valCompra;
+		this.valVenda = valVenda;
+		this.dataEntrada = dataEntrada;
+		this.quantida = quantida;
 	}
 
 	public String getNomePeca() {
@@ -31,6 +43,66 @@ public class Peca {
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
+
+	public double getValCompra() {
+		return valCompra;
+	}
+
+	public void setValCompra(double valCompra) {
+		this.valCompra = valCompra;
+	}
+
+	public double getValVenda() {
+		return valVenda;
+	}
+
+	public void setValVenda(double valVenda) {
+		this.valVenda = valVenda;
+	}
+
+	public String getDataEntrada() {
+		return dataEntrada;
+	}
+
+	public void setDataEntrada(String dataEntrada) {
+		this.dataEntrada = dataEntrada;
+	}
+
+	public String getDataSaida() {
+		return dataSaida;
+	}
+
+	public void setDataSaida(String dataSaida) {
+		this.dataSaida = dataSaida;
+	}
+
+	public int getQuantida() {
+		return quantida;
+	}
+
+	public void setQuantida(int quantida) {
+		this.quantida = quantida;
+	}
+
+	public int getIdPeca() {
+		return idPeca;
+	}
+
+	public void setIdPeca(int idPeca) {
+		this.idPeca = idPeca;
+	}
+
+	public int getIdItempeca() {
+		return idItempeca;
+	}
+
+	public void setIdItempeca(int idItempeca) {
+		this.idItempeca = idItempeca;
+	}
+	
+	
+	
+	
 	
 	
 
